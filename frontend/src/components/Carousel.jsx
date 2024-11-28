578b72dd022c4f5d454ace804d85bd8213c17349
jhndjx <jhndjx@protonmail.com>
2024-11-28 20:06:00 +0100

    Gestion de l'affichage des flèches de navigation en fonction du nombre d'images

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,8 +1,9 @@
-import {useState} from 'react';
+import { useState, useMemo } from 'react';
 import '../assets/css/Carousel.css';
 
-function Carousel({images}) {
+function Carousel({ images }) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const hasMultipleImages = useMemo(() => images.length > 1, [images.length]);
 
     const goToPrevious = () => {
         const isFirstSlide = currentIndex === 0;
@@ -16,15 +17,26 @@ function Carousel({images}) {
         setCurrentIndex(newIndex);
     };
 
+    if (images.length === 0) {
+        return <div className="carousel">Aucune image</div>;
+    }
+
     return (
         <div className="carousel">
-            <button className="carousel-left" onClick={goToPrevious}>
-                <i className="fas fa-chevron-left"></i>
-            </button>
-            <img src={images[currentIndex]} alt="Carousel" className="carousel-image"/>
-            <button className="carousel-right" onClick={goToNext}>
-                <i className="fas fa-chevron-right"></i>
-            </button>
+            {hasMultipleImages && (
+                <button className="carousel-left" onClick={goToPrevious}>
+                    <i className="fas fa-chevron-left"></i>
+                </button>
+            )}
+
+            <img src={images[currentIndex]} alt={`Carousel, image n°${currentIndex + 1}`} className="carousel-image" />
+
+            {hasMultipleImages && (
+                <button className="carousel-right" onClick={goToNext}>
+                    <i className="fas fa-chevron-right"></i>
+                </button>
+            )}
+
             <div className="carousel-indicator">
                 {currentIndex + 1}/{images.length}
             </div>
